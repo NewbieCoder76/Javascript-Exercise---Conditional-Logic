12b84f6153aa0f0cddf4ed731850b4ba4e5ea4aa
NewbieCoder76 <w.mxabanisi2003@gmail.com>
2025-10-31 14:25:53 +0200

    Meatloaf

---
+++ basics.js
@@ -0,0 +1,14 @@
+// Create a variable called score and give it a value between 0 and 100
+let score = 67;
+
+// Create a variable called passingGrade and set it to 60
+let passingGrade = 60;
+
+// Write an if statement to check if score is greater than or equal to passingGrade
+if (score === 100) {
+    console.log("Perfect score!");
+} else if (score >= passingGrade) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+}
